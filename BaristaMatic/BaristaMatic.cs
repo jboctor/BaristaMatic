1eb414f95b8da99560b69b35419caffd91cb1fe8
John Boctor <jboctor@outlook.com>
2019-03-12 10:21:28 -0500

    Initial init. New BaristaMatic project

---
+++ BaristaMatic/BaristaMatic.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BaristaMatic
+{
+    class BaristaMatic
+    {
+        public BaristaMatic()
+        {
+        }
+    }
+}
---
+++ BaristaMatic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BaristaMatic
+{
+    class Program
+    {
+        string input;
+        static void Main(string[] args)
+        {
+            BaristaMatic = new BaristaMatic();
+            
+            do {
+                input = Console.ReadLine();
+                bool valid = isInputValid(input);
+            } while (input.ToLower() != "q");
+        }
+
+        static bool isInputValid(string input)
+        {
+
+        }
+    }
+}
